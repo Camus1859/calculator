1db91120bec425e3cf4e853e4086322f3a0bf8ab
Anderson Laventure <Laventurepsu1@gmail.com>
2020-06-09 19:58:26 -0400

    updated CSS

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function add(x, y) {
+  return x + y
+};
+
+function subtract(x, y){
+  return x - y
+}
+
+function multiply() {
+  return x * y
+}
+
+function divide(){
+  return x / y
+}
+
+function operate(operator, x, y) {
+
+  return operator(x, y)
+}
