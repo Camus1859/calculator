baa285a2f89f4d5456c72ece10b540feb2e716f4
Anderson Laventure <Laventurepsu1@gmail.com>
2020-06-12 16:35:41 -0400

    updated operators

--- calculator.js
+++ calculator.js
@@ -21,129 +21,67 @@ function operate(operator, x, y) {
 
 let screen = document.querySelectorAll(".screen")
 let allDigits = ""
-let number = []
 let digits = document.querySelectorAll(".digits");
 let operators = document.querySelectorAll(".operators")
-let screenLength;
-let equal = document.querySelectorAll(".equal")
+let equal = document.getElementById("equal")
 let prevNumOnScreen = 0
 let currentNUmOnScreen = 0
 let displayValue = 0
+let newArray = []
 
 digits.forEach(element => {
   element.addEventListener("click", digitOnScreen) 
    function digitOnScreen() { 
-     let digitClicked = Number(element.textContent)
-     number.push(digitClicked)
-     let iterable = number.values()
-     for (let value of iterable) {
-      allDigits = allDigits + value
-      number = []
-      displayValue = document.getElementById('screen').textContent =  Number(allDigits)
-      let newArray = []
+     let digitClicked = (element.textContent)
+      allDigits = allDigits + digitClicked
+      displayValue = document.getElementById('screen')
+      displayValue.textContent =  Number(allDigits)
+      newArray = []
       newArray.push(displayValue)
-      screenLength = newArray.join("").split("").length
-
-      
-    
-   
       document.getElementById("screen").style.color = "black"
-     }
    }
 })
 
 
 operators.forEach(element => {
   element.addEventListener("click", function (e){
-    if (screenLength > 0) {
-
-    oper = (e.target.classList[1])
-
-  }
-    prevNumOnScreen = displayValue
-
-    document.getElementById('screen').textContent =  Number(allDigits) * 0
-    
-    allDigits = 0
-
-
-
-
-    equal.forEach(element => {
-    element.addEventListener("click", function (e){
-
+    if (newArray.length > 0) {
+      operating = (e.target.classList[1])
+      prevNumOnScreen = Number(allDigits)
       allDigits = 0
-
-
-     currentNUmOnScreen = displayValue
+    }
     
+      equal.addEventListener("click", function (e){
+        currentNUmOnScreen = Number(allDigits)
+      
+        if (operating == "add") {
+          total = document.getElementById('screen')
+          total.textContent =  add(currentNUmOnScreen, prevNumOnScreen)
+        }
 
-     if (oper == "add") {
+        else if (operating == "subtract") {
+          total = document.getElementById('screen')
+          total.textContent =  subtract(currentNUmOnScreen, prevNumOnScreen)
+        }
+        
+        else if (operating == "multiply") {
+          total = document.getElementById('screen')
+          total.textContent =  multiply(currentNUmOnScreen, prevNumOnScreen)
+        }
+        
+        else if (operating == "divide") {
+          total = document.getElementById('screen')
+          total.textContent =  divide(currentNUmOnScreen, prevNumOnScreen)
 
+        } 
+      }) 
+  })
+})
 
-     total  = document.getElementById('screen').textContent =  add(currentNUmOnScreen, prevNumOnScreen)
-     console.log(total)
-
-
-
-      }
-      else if (e.target.classList[1] == "subtract") {
-  
-      let subtracting = e.target.classList[1]
-      }
-      else if (e.target.classList[1]== "multiply") {
-  
-      let multiplying = e.target.classList[1]
-      }
-      else if (e.target.classList[1] == "divide") {
-        let dividing = e.target.classList[1]
-      } 
-    })})
-
- 
-
-  })})
-
-    //  if(operaate == "add"){
-    //    operaate = add()
-    //  }
-    //  else if(operaate == "subtract") {
-    //    operaate = subtract()
-    //  }
-    //  else if(operaate == "divide"){
-    //    operaate = divide()
-    //  }
-    //  else if(operaate == "multiply") {
-    //    operaate = multiply()
-    //  }
-
-    // }
-    // equal.addEventListener("click", function(){
-
-
-    // })
+   
 
 
 
 
-  // })
-// });
+
     
-// e.target.classList[1] == "add" || e.target.classList[1] == "subtract" || e.target.classList[1] == "divide"  || e.target.classList[1] == "multiply" && 
-
-
-    // if (e.target.classList[1] == "add") {
-
-    // let adding = e.target.classList[1]
-    // }
-    // else if (e.target.classList[1] == "subtract") {
-
-    // let subtracting = e.target.classList[1]
-    // }
-    // else if (e.target.classList[1]== "multiply") {
-
-    // let multiplying = e.target.classList[1]
-    // }
-    // else if (e.target.classList[1] == "divide") {
-    //   let dividing = e.target.classList[1]
-    // }
